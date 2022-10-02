663912c4627ab70626f3504d8796473d8576530c
SYANH MACAIR <syanh2512@gmail.com>
2022-10-02 13:24:42 +0900

    add file encoder.py

---
+++ encoder.py
@@ -0,0 +1,13 @@
+import joblib
+import torch
+import torch.nn as nn
+
+class Encoder():
+    def __init__(self) -> None:
+        super(Encoder, self).__init__
+        
+
+
+    def forward(self, x):
+        pass
+
