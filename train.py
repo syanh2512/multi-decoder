f8116c50293788af07676fb6350c4f2ab579d00f
SYANH GIGABYTE <syanh2512@gmail.com>
2022-10-07 02:30:27 +0900

    edit some changes

--- train.py
+++ train.py
@@ -1,4 +1,6 @@
 import torch
+import torch.nn as nn
+import joblib
 
 
 
@@ -6,4 +8,8 @@ if torch.cuda.is_available():
     device = torch.device("cuda")
 else:
     device = torch.device("cpu")
+
+
+hidden_vector_dict = joblib.load("jissen/results/CIDDS-001/hidden-vector.joblib")
+print(len(hidden_vector_dict))
 
